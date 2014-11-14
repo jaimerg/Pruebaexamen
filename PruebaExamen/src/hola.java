fd9b6ee3c3cc86a18d9e7ac55a210cbbebe45d4c
Jaime <Jaime@Jaime-PC.com>
2014-11-14 11:41:01 +0100

    hola

---
+++ PruebaExamen/src/hola.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class hola {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception{
+		System.out.println("dime un numero");
+		int numero = Integer.parseInt(br.readLine());
+		
+		System.out.println(numero);
+	}
+
+}
